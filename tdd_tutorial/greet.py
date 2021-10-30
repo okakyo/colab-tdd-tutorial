61e87de744508e8a1fd034dff35c9fd79d2dbb71
okakyo <oka@logly.co.jp>
2021-10-30 14:27:13 +0900

    q1: 仮の答えを追加

--- tdd_tutorial/greet.py
+++ tdd_tutorial/greet.py
@@ -1,8 +1,13 @@
+from datetime import datetime,time
 
 def greeting():
-  
-  # この関数に、仕様を満たすように関数を装してください。 
-  return "こんにちは"
+  # この関数に、仕様を満たすように関数を装してください。
+  nowTime = datetime.now().time()
+  if (time(5,0,0)<= nowTime and nowTime < time(12,0,0)):
+    return "おはようございます"
+  elif(time(12,0,0) <=nowTime and nowTime < time(18,0,0)):
+    return "こんにちは"
+  return "こんばんは"
 
 if __name__ == "__main__":
     print(greeting())
