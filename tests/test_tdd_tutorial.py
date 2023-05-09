a837f6a81e73ed0339fe187d5e7689e53cbe1426
okakyo <oka@logly.co.jp>
2023-05-09 17:55:06 +0900

    fix test style

--- tests/test_tdd_tutorial.py
+++ tests/test_tdd_tutorial.py
@@ -1,12 +1,16 @@
 from tdd_tutorial import __version__, Person
-from datetime import datetime
+import datetime
 import pytest
 
 def test_version():
     assert __version__ == '0.1.0'
 
-@pytest.mark.freeze_time(datetime(2000, 1, 2, 3, 4))
-def test_greeting():
-    person  = Person()
-    greeting_word = person.greeting()
+@pytest.fixture
+def mock_person():
+  mock_person = Person()
+  return mock_person
+
+@pytest.mark.freeze_time(datetime.datetime(2000, 1, 2, 3, 4))
+def test_greeting(mock_person):
+    greeting_word = mock_person.greeting()
     assert(greeting_word =="こんばんは")
